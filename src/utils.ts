24f33edeb2589e17cc3a5f5c63c969f0bb3e2944
wangwei <wei.wang@sorcara.com>
2024-06-17 16:41:15 +0800

    ui

--- src/utils.ts
+++ src/utils.ts
@@ -19,11 +19,34 @@ export const detectType = (b64: string): Type | undefined => {
   }
 }
 
-export const formatCurrentDate = (): string => {
-  const today = new Date();
+export const formatCurrentDate = (today: Date): string => {
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0');
   return `${year}/${month}/`;
+}
+
+export const formatFileName = (fileName: string, now: Date): string => {
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+
+  // 格式化时间戳
+  const timestamp = `${hours}${minutes}${seconds}${milliseconds}`;
+
+  // 查找文件后缀
+  const lastDotIndex = fileName.lastIndexOf('.');
+  if (lastDotIndex === -1) {
+    // 如果没有文件后缀，直接添加时间戳
+    return `${fileName}_${timestamp}`;
+  }
+
+  // 分割文件名和文件后缀
+  const name = fileName.substring(0, lastDotIndex);
+  const extension = fileName.substring(lastDotIndex);
+
+  // 生成新的文件名
+  return `${name}_${timestamp}${extension}`;
 }
 
 export function getImageInfo(b64: string) {
